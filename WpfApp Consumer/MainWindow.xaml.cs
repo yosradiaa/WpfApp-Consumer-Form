ab966abcd4a35b1125f1537030f4ba1c2b66bba2
yosradiaa <yossradiaa133@gmail.com>
2024-02-28 22:47:56 +0200

    Add project files.

---
+++ WpfApp Consumer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp_Consumer.Models;
+
+namespace WpfApp_Consumer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage res= await httpClient.GetAsync("http://localhost:5037/api/Department");
+            if (res.IsSuccessStatusCode)
+            {
+                string msg=await res.Content.ReadAsStringAsync();
+                List<Department> departments = JsonSerializer.Deserialize<List<Department>>(msg)??new List<Department>();
+                DeptList.ItemsSource = departments;
+            }
+            else
+            {
+                MessageBox.Show("TRY AGAIN");
+            } 
+                
+        }
+    }
+}
---
+++ WpfApp Consumer/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Consumer.Models
+{
+    public class Department
+    {
+        public string name {  get; set; }
+        public string manager { get; set; }
+        public string location { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"{name}";
+        //}
+    }
+}
